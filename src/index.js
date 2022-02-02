6290ec6261c6673fdc69aef75b4c403ec3d4c4bd
itssabhishek <itsabhishek0710@gmail.com>
2022-02-02 21:24:25 +0530

    Added Preloader

--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import Preloader from "./Preloader";
+import Navbar from "./Navbar/Navbar";
 import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
 import "../node_modules/bootstrap/dist/js/bootstrap";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <Preloader />
-  </React.StrictMode>,
-  document.getElementById("root")
-);
+ReactDOM.render(<React.StrictMode>
+    <>
+        <Preloader/>
+        <Navbar/>
+    </>
+</React.StrictMode>, document.getElementById("root"));
